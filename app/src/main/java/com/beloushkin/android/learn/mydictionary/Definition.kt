ea1064a97984f1ecdace041a3ace32f092c5ceca
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-05-14 17:44:23 +0300

    Adds initial layout to app

---
+++ app/src/main/java/com/beloushkin/android/learn/mydictionary/Definition.kt
@@ -0,0 +1,12 @@
+package com.beloushkin.android.learn.mydictionary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Definition : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_definition)
+    }
+}
