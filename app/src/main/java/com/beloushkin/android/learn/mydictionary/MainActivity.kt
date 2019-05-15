6f93ea9dc71cd387564038a4b0011ace2b6c2bc8
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-05-15 16:51:57 +0300

    Adds feature extraction from JSON, INTERNET permission

--- app/src/main/java/com/beloushkin/android/learn/mydictionary/MainActivity.kt
+++ app/src/main/java/com/beloushkin/android/learn/mydictionary/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONException
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
@@ -26,20 +28,53 @@ class MainActivity : AppCompatActivity() {
     fun findWord(view: View) {
         var stringUrl =
             "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" + edtWord.text.toString()
+
+        MyAsyncTask().execute(stringUrl)
     }
 
     inner class MyAsyncTask : AsyncTask<String, Void, Data>() {
-        override fun doInBackground(vararg params: String?): Data {
+        override fun doInBackground(vararg params: String?): Data? {
             val url = createUrl(params[0])
 
             var jsonResponse: String?
-            jsonResponse = getHttpResponse(url)
+            try {
+                jsonResponse = getHttpResponse(url)
+                val data = extractFeatureFromJson(jsonResponse)
+                return data
+            } catch (e: IOException) {
+                Log.e("MainActivity", "Error while getting response")
+            }
+            return null
         }
 
         override fun onPostExecute(result: Data?) {
             super.onPostExecute(result)
         }
 
+    }
+
+    fun extractFeatureFromJson(definitionJson: String?):Data?{
+        try {
+            val baseJsonResponse = JSONObject(definitionJson)
+            val definitions = baseJsonResponse
+                .getJSONArray("results")!!
+                .getJSONObject(0)!!
+                .getJSONArray("lexicalEntries")!!
+                .getJSONObject(0)!!
+                .getJSONArray("entries")!!
+                .getJSONObject(0)!!
+                .getJSONArray("senses")!!
+                .getJSONObject(0)!!
+                .getJSONArray("definitions")
+
+            Log.d("Definition", definitions[0].toString())
+
+            return Data( definitions[0].toString())
+
+        } catch (e: JSONException) {
+            Log.e("MainActivity", "Error while parsing JSON")
+            return null
+        }
     }
 
     fun getHttpResponse(url: URL?):String {
@@ -70,10 +105,7 @@ class MainActivity : AppCompatActivity() {
         } catch (e: IOException) {
             Log.e("MainActivity","Connection error: " + e)
         }
-
-
-
-
+        return jsonResponse
     }
 
     fun readFromInputStream(inputStream: InputStream): String {
